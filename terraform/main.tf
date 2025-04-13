985fff89eb7f232f577b1fb1512705cd82887f2e
Bee <beta.jurg@gmail.com>
2025-04-13 12:16:34 +0100

    docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,13 @@ resource "docker_image" "docker_build" {
 resource "docker_registry_image" "demo" {
   name = docker_image.docker_build.name
 }
+
+resource "docker_container" "java_web_app_container" {
+  name  = "java-web-app-container"
+  image = docker_image.docker_build.name  # Using the image built above
+  ports {
+    internal = 8080  # Exposed port inside the container
+    external = 8080  # Exposed port on the host machine
+  }
+  restart = "always"  # Optionally, to ensure it restarts if it fails
+}
